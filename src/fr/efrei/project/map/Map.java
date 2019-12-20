b5c5476974bf3730b87961b07bb2c6e982821673
Rémi <remi.cabo.broseta@gmail.com>
2019-12-20 22:28:44 +0100

    Improve map generation & player UI

--- src/fr/efrei/project/map/Map.java
+++ src/fr/efrei/project/map/Map.java
@@ -8,58 +8,69 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Random;
 
-public class Map
-{
-
+public class Map {
     private Case[][] listCase;
     private String name;
 //    private HashMap<Player, ArrayList<Case>> listOwnedLand;
 
-    public Map(int size)
-    {
+    public Map(int size) {
         this.listCase = new Case[size][size];
         this.name = MapName.values()[new Random().nextInt(MapName.values().length)].toString();
 //        this.listOwnedLand = new HashMap<>();
     }
 
-    public void initMap(Player[] p, HashMap<Player, HashSet<Case>> listOwnedLand)
-    {
+    public void initMap(Player[] p, HashMap<Player, HashSet<Case>> listOwnedLand) {
         Random r = new Random();
         int tmp;
+        int maxLand = listCase.length * listCase[0].length;
+        int maxLandByP = maxLand / (p.length + 1);
+        int[] landByP = new int[p.length];
 
         /*
          * Attribution des territoires aux joueurs
          */
 
-        for(int i = 0; i < listCase.length; i++)
-        {
-            for(int j = 0; j < listCase[i].length; j++)
-            {
+        for (int i = 0; i < listCase.length; i++) {
+            for (int j = 0; j < listCase[i].length; j++) {
                 tmp = r.nextInt(p.length + 1);
                 /*
                  * Le territoire n'est attribué à personne
                  */
-                if(tmp > p.length - 1)
-                {
-                    listCase[i][j] = new Case();
-                }
-                else
-                {
 
-                    try {
-                        listCase[i][j] = new Case(p[tmp]);
-                        HashSet<Case> tmpList = new HashSet<>();
+                if (tmp < p.length) {
+                    while (true) {
+                        if (landByP[tmp] < maxLandByP) {
+                            try {
+                                listCase[i][j] = new Case(p[tmp]);
+                                HashSet<Case> tmpList = new HashSet<>();
 
-                        if(listOwnedLand.containsKey(p[tmp]))
-                        {
-                            tmpList = listOwnedLand.get(p[tmp]);
+                                if (listOwnedLand.containsKey(p[tmp])) {
+                                    tmpList = listOwnedLand.get(p[tmp]);
+                                }
+                                tmpList.add(getCaseFromIndex(this, i, j));
+                                listOwnedLand.put(p[tmp], tmpList);
+
+                                landByP[tmp] += 1;
+                                break;
+                            } catch (UnknownCaseInMap unknownCaseInMap) {
+                                unknownCaseInMap.printStackTrace();
+                                break;
+                            }
                         }
-                        tmpList.add(getCaseFromIndex(this, i, j));
-                        listOwnedLand.put(p[tmp], tmpList);
-
-                    } catch (UnknownCaseInMap unknownCaseInMap) {
-                        unknownCaseInMap.printStackTrace();
+                        else{
+                            boolean valid = true;
+                            for(int l : landByP){
+                                valid = (valid && l == maxLandByP);
+                            }
+                            if(valid){
+                                listCase[i][j] = new Case();
+                                break;
+                            }
+                            tmp = r.nextInt(p.length);
+                        }
                     }
+                } else {
+                    listCase[i][j] = new Case();
                 }
             }
         }
@@ -67,7 +78,7 @@ public class Map
     }
 
     public static Case getCaseFromIndex(Map map, int x, int y) throws UnknownCaseInMap {
-        if(x < map.getListCase().length && y < map.getListCase()[0].length) {
+        if (x < map.getListCase().length && y < map.getListCase()[0].length) {
             return map.getListCase()[x][y];
         } else {
             throw new UnknownCaseInMap();
@@ -75,19 +86,19 @@ public class Map
     }
 
     public static Case getCaseFromId(Map map, int id) throws UnknownCaseInMap {
-        if(id > 0 && id < Math.pow(map.getListCase().length, 2)) {
+        if (id > 0 && id < Math.pow(map.getListCase().length, 2)) {
             Integer[] index = Map.getIndexFromId(map, id);
+/*
             System.out.println("getCaseFromId id: " + id);
             System.out.println(index[0] + " " + index[1]);
+*/
             return map.getListCase()[index[0]][index[1]];
-        } else
-        {
+        } else {
             throw new UnknownCaseInMap();
         }
     }
 
-    public static Integer[] getIndexFromId(Map map, int id)
-    {
+    public static Integer[] getIndexFromId(Map map, int id) {
         Integer[] coordinates = new Integer[2];
 
         coordinates[0] = id / (map.getListCase().length);
@@ -97,49 +108,35 @@ public class Map
 
     }
 
-    public static ArrayList<Case> getNeighbors(Map map, int id)
-    {
+    public static ArrayList<Case> getNeighbors(Map map, int id) {
         ArrayList<Case> neighbors = new ArrayList<>();
 
         Integer[] index = Map.getIndexFromId(map, id);
         int x = index[0];
         int y = index[1];
 
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+
         //to refactor
 
-        if(x > 0)
-        {
-            //left
-            if(y > 0)
-            {
-                neighbors.add(map.getListCase()[x - 1][y - 1]);
-                //top
-            }
-            if(y < 8)
-            {
-                neighbors.add(map.getListCase()[x - 1][y + 1]);
-                //bottom
-            }
+        if (x > 0) {
+            neighbors.add(map.getListCase()[x - 1][y]);
         }
-        if(x < 8)
+        if(x < 8) {
+            neighbors.add(map.getListCase()[x + 1][y]);
+        }
+        if(y > 0)
         {
-            //right
-            if(y > 0)
-            {
-                neighbors.add(map.getListCase()[x + 1][y - 1]);
-                //top
-            }
-            if(y < 8)
-            {
-                neighbors.add(map.getListCase()[x + 1][y + 1]);
-                //bottom
-            }
+            neighbors.add(map.getListCase()[x][y - 1]);
+        }
+        if (y < 8) {
+            neighbors.add(map.getListCase()[x][y + 1]);
         }
         return neighbors;
     }
 
-    public static ArrayList<Case> getNeighbors(Map map, Case c)
-    {
+    public static ArrayList<Case> getNeighbors(Map map, Case c) {
         return Map.getNeighbors(map, c.getId());
     }
 
@@ -155,11 +152,9 @@ public class Map
     public String toString() {
         StringBuilder tmp = new StringBuilder("Carte: " + name + "\n");
 
-        for(Case[] tabCase : this.listCase)
-        {
+        for (Case[] tabCase : this.listCase) {
             tmp.append("[ ");
-            for(Case c : tabCase)
-            {
+            for (Case c : tabCase) {
                 tmp.append("(").append(c).append(") \t");
             }
             tmp.append("]\n");
