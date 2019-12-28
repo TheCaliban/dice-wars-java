8bc9321a27d6c40ea477559462a210d8feb21522
Rémi <remi.cabo-broseta@efrei.net>
2019-12-28 22:08:53 +0100

    Implements base for future IA

---
+++ src/fr/efrei/project/ia/Robot.java
@@ -0,0 +1,16 @@
+package fr.efrei.project.ia;
+
+import fr.efrei.project.player.Player;
+
+public class Robot {
+
+    private String difficulty;
+    private Player player;
+
+    public Robot(String difficulty, Player player)
+    {
+        this.difficulty = difficulty;
+        this.player = player;
+    }
+
+}
